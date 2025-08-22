78ca0776d026c8cf0ea5b94379101cb5bbe75bb4
Stellar2703 <rajammalshash@gmail.com>
2025-08-23 00:26:47 +0530

    updated alb

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://127.0.0.1:3000',,'http://proj-alb-470887536.ap-south-1.elb.amazonaws.com'],
+  origin: ['http://lb-project-1657527018.ap-south-1.elb.amazonaws.com'],
   credentials: true,
   optionsSuccessStatus: 200,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
